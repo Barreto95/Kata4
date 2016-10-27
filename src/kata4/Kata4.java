8dc81a67f052d49eb69e6676446ffb358293ba2b
usuario <usuario@10.22.143.42>
2016-10-27 11:46:41 +0100

    Kata4 v3	"Devuelve las direcciones"

--- src/kata4/Kata4.java
+++ src/kata4/Kata4.java
@@ -1,20 +1,17 @@
 package kata4;
 
 import java.io.File;
-
+import java.util.ArrayList;
+import java.io.IOException;
 public class Kata4 {
 
-    public static void main(String[] args) {
-        File file = new File("c:\\pub\\repositorio");
-        print(file.listFiles(),"");
+    public static void main(String[] args) throws IOException {
+        String fileName = "C:\\Users\\usuario\\Documents\\NetBeansProjects\\Kata4\\emailsfilev1.txt";
+        ArrayList<String> mailList = MailListReader.read(fileName);
+        Histogram<String> histogram = MailHistogramBuilder.build(mailList);
+        HistogramDisplay histoDisplay = new HistogramDisplay(histogram);
+        histoDisplay.execute();
     }
     
-    public static void print(File[] files, String indent){
-        if(files == null) return;
-        for (File file : files) {
-            System.out.println(indent + (file.isDirectory()? "+" : "-") + file.getName());
-            if(!file.isDirectory() || file.isHidden()) continue;
-            print(file.listFiles()," ");
-        }
-    }
+    
 }
